a28230d83463b31003b012247f7402534642e7f1
muhammadabdullohuzinfocom <muhammadabdulloh.uzinfocom@gmail.com>
2022-04-25 11:45:02 +0500

    *ngFor dan foydalanishni o'rgandik array bilan birgalikda

--- src/app/lessons.component.ts
+++ src/app/lessons.component.ts
@@ -8,8 +8,25 @@ import { Component } from "@angular/core";
     //bu usul mo'ylovga o'xshagani uchun mustache syntax deb ham ataladi
 
     //buyerada View title o'zgaruvchisiga binding qilinmoqda
-    template: '{{ "Sarlavha: " + title}}'
+
+    // back tick yani bek tik ( => ` <= ) bunday belgi nima uchun qo'yiladi desak
+    // bunday holda biz bir nechta qator satrlarni yozishimiz mumkin huddi pythondagi """  """ shunga o'xshedi ekan :)
+    template: `
+    <h1>{{getTitle()}}</h1>
+
+    <ul>
+        <li *ngFor="let lesson of lessonsArray">
+            {{lesson}}
+        </li>
+    </ul>
+    `
 })
 export class LessonsComponent {
     title: string = "Darslar ro'yhati";
+
+    getTitle(): string {
+        return  "Sarlavha: " + this.title;
+    }
+
+    lessonsArray = ["Angular asoslari", "Web Api", "Entity Framework", "MS SQL Server"];
 }
