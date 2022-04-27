8464c9cc3b27981f5605bd22f5efb241274071fd
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2022-04-27 15:09:56 +0500

    create 3 components

---
+++ src/app/page-note-found/page-note-found.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageNoteFoundComponent } from './page-note-found.component';
+
+describe('PageNoteFoundComponent', () => {
+  let component: PageNoteFoundComponent;
+  let fixture: ComponentFixture<PageNoteFoundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PageNoteFoundComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageNoteFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
